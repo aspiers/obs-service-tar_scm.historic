9312670eb1e03e8ef6e68f5ebb3c4caab7bdd6ad
Adam Spiers <aspiers@suse.com>
2012-02-07 16:04:18 +0100

    improve 'keep-source', add 'versionformat' parameter, and a unit test suite

---
+++ test/gittests.py
@@ -0,0 +1,223 @@
+#!/usr/bin/python
+
+import os
+import subprocess
+import unittest
+import shutil
+import sys
+
+from procutils import quietrun
+from testsbase import TestsBase
+
+def create_git_commits(start, end):
+    subdir = 'subdir'
+    if not os.path.exists(subdir):
+        os.mkdir(subdir)
+    for i in xrange(start, end+1):
+        for fn in ('a', subdir + '/b'):
+            f = open(fn, 'w')
+            f.write(str(i))
+            f.close()
+        quietrun('git add .')
+        quietrun('git commit -m%d' % i)
+
+def run_git(repo, opts):
+    return subprocess.check_output('cd %s && git %s' % (repo, opts), shell=True)
+
+class GitTests(TestsBase):
+    tests_dir    = os.path.abspath(os.path.dirname(__file__)) # os.getcwd()
+    tmpdir       = tests_dir + '/tmp'
+    fixtures_dir = tmpdir + '/fixtures'
+    repo_dir     = fixtures_dir + '/repo.git'
+    repo_url     = 'file://' + repo_dir
+
+    tar_scm_bin  = tests_dir + '/../tar_scm'
+    if not os.access(tar_scm_bin, os.X_OK):
+        raise RuntimeError, "Failed to find tar_scm at " + tar_scm_bin
+
+    @classmethod
+    def setup_fixtures(cls):
+        if os.path.exists(cls.fixtures_dir):
+            shutil.rmtree(cls.fixtures_dir)
+        os.mkdir(cls.fixtures_dir)
+
+        cls.init_git_repo(cls.repo_dir)
+
+        cls.timestamps   = { }
+        cls.sha1s        = { }
+
+        create_git_commits(1, 10)
+        cls.record_git_tag('ten')
+
+        create_git_commits(11, 20)
+        cls.record_git_tag('twenty')
+
+        create_git_commits(21, 30)
+        cls.record_git_tag('thirty')
+
+        cls.reset_upstream('ten')
+
+    @classmethod
+    def init_git_repo(cls, repo):
+        os.makedirs(repo)
+        os.chdir(repo)
+        quietrun('git init')
+        quietrun('git config user.name test')
+        quietrun('git config user.email test@test.com')
+
+    @classmethod
+    def record_git_tag(cls, tag):
+        quietrun('git tag ' + tag)
+        cls.timestamps[tag] = cls.get_git_metadata('%at')
+        cls.sha1s[tag]      = cls.get_git_metadata('%h')
+
+    @classmethod
+    def get_git_metadata(cls, formatstr):
+        return run_git(cls.repo_dir, 'log -n1 --pretty=format:%s' % formatstr)
+
+    @classmethod
+    def reset_upstream(cls, tag):
+        run_git(cls.repo_dir, 'reset --hard %s 2>&1' % tag)
+
+    def stdargs(self, *args):
+        return [ '--url', self.repo_url, '--scm', 'git' ] + list(args)
+
+    def assertPkgWorkingDir(self, name, numcommits, sha1):
+        wd = os.path.join(self.pkgdir, name)
+        self.assertTrue(os.path.exists(wd), '%s did not exist' % wd)
+        self.assertTrue(os.path.isdir(wd),  '%s was not a dir' % wd)
+
+        out = run_git(wd, 'log --pretty=oneline | wc -l')
+        self.assertRegexpMatches(out, '^\d+$')
+        self.assertEqual(int(out), numcommits)
+
+        out = run_git(wd, 'rev-parse --short HEAD')
+        self.assertEqual(out, sha1 + "\n")
+
+    def test_plain(self):
+        self.tar_scm_std()
+        basename = 'repo-%s' % self.timestamps['ten']
+        self.assertTarOnly(basename)
+
+    def test_exclude_git(self):
+        self.tar_scm_std('--exclude', '.git')
+        basename = 'repo-%s' % self.timestamps['ten']
+        self.assertTarOnly(basename)
+
+    def test_subdir(self):
+        self.tar_scm_std('--subdir', 'subdir')
+        basename = 'repo-%s' % self.timestamps['ten']
+        self.assertTarOnly(basename, tarchecker=self.assertSubdirTar)
+
+    def test_filename(self):
+        self.tar_scm_std('--filename', 'myfilename')
+        basename = 'myfilename-%s' % self.timestamps['ten']
+        self.assertTarOnly(basename)
+
+    def test_version(self):
+        version = '0.5'
+        self.tar_scm_std('--version', version)
+        basename = 'repo-%s' % version
+        self.assertTarOnly(basename)
+
+    def test_filename_version(self):
+        version = '0.6'
+        filename = 'myfilename'
+        self.tar_scm_std('--filename', filename, '--version', version)
+        basename = filename + '-' + version
+        self.assertTarOnly(basename)
+
+    def test_versionformat_abbrevhash(self):
+        self.tar_scm_std('--versionformat', '%h')
+        basename = 'repo-%s' % self.sha1s['ten']
+        self.assertTarOnly(basename)
+
+    def test_versionformat_timestamp(self):
+        self.tar_scm_std('--versionformat', '%at')
+        basename = 'repo-%s' % self.timestamps['ten']
+        self.assertTarOnly(basename)
+
+    def test_versionformat_mixed(self):
+        self.tar_scm_std('--versionformat', '%at.master.%h')
+        basename = 'repo-%s.master.%s' % (self.timestamps['ten'], self.sha1s['ten'])
+        self.assertTarOnly(basename)
+
+    # FIXME: currently no way of testing that this did the right
+    # thing, because the cloned repo is removed before the source
+    # service terminates.  To test this properly, we'd need to wrap or
+    # stub the git executable by prepending to $PATH.
+    def test_history_depth(self):
+        self.tar_scm_std('--history-depth', '1')
+        basename = 'repo-%s' % self.timestamps['ten']
+        self.assertTarOnly(basename)
+
+    # FIXME: same issue here as above
+    def test_history_depth_full(self):
+        self.tar_scm_std('--history-depth', 'full')
+        basename = 'repo-%s' % self.timestamps['ten']
+        self.assertTarOnly(basename)
+
+    def test_keep_source_incompatible_with_history_depth(self):
+        args = [
+            '--keep-source', 'true',
+            '--history-depth', '10',
+            '--version', '4.0',
+        ]
+        (stdout, stderr, ret) = self.tar_scm_std_fail(*args)
+        self.assertRegexpMatches(stdout, '--keep-source is incompatible with shallow clones via --history-depth')
+
+    def test_keep_source_invalid_arg(self):
+        self.tar_scm_std_fail('--keep-source', 'lksjdf', '--version', '1.1')
+
+    def test_keep_source_requires_version(self):
+        (stdout, stderr, ret) = self.tar_scm_std_fail('--keep-source', 'true')
+        self.assertRegexpMatches(stdout, '--keep-source requires --version')
+
+    def test_keep_source(self):
+        self.sequential_runs('3.0')
+
+    def sequential_runs(self, version, should_fail=None):
+        """
+        Runs tar_scm three times in a row, each with potentially
+        different parameters, to test reuse of a source tree left by
+        --keep-source.
+        """
+        self.mkfreshdir(self.pkgdir)
+
+        args = [
+            '--keep-source', 'true',
+            '--version', version
+        ]
+        basename = 'repo-%s' % version
+
+        def _check_subsequent_run(self, depth, tag):
+            self.tar_scm_std(*args)
+            self.assertTarOnly(basename)
+            self.postRun()
+            self.assertPkgWorkingDir(basename, depth, self.sha1s[tag])
+
+        # first run
+        self.tar_scm_std(*args)
+        self.assertTarAndDir(basename)
+        self.postRun()
+        self.assertPkgWorkingDir(basename, 10, self.sha1s['ten'])
+
+        _check_subsequent_run(self, 10, 'ten')
+        self.reset_upstream('twenty')
+        _check_subsequent_run(self, 20, 'twenty')
+        _check_subsequent_run(self, 20, 'twenty')
+        self.reset_upstream('thirty')
+        _check_subsequent_run(self, 30, 'thirty')
+
+    def test_version_versionformat(self):
+        self.mkfreshdir(self.pkgdir)
+        self.tar_scm_std('--version', '3.0', '--versionformat', '%at.master.%h')
+        basename = 'repo-%s.master.%s' % (self.timestamps['ten'], self.sha1s['ten'])
+        self.assertTarOnly(basename)
+
+    def test_version_versionformat_keep_source(self):
+        self.mkfreshdir(self.pkgdir)
+        version = '3.1'
+        self.tar_scm_std('--version', version, '--versionformat', '%at.master.%h', '--keep-source', 'true')
+        basename = 'repo-%s.master.%s' % (self.timestamps['ten'], self.sha1s['ten'])
+        self.assertTarAndDir(basename, dirname='repo-%s' % version)
---
+++ test/testsbase.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python
+
+import os
+import shutil
+import tarfile
+import unittest
+from pprint import pprint, pformat
+from procutils import shexec, run_cmd
+
+class TestsBase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.setup_fixtures()
+
+    def mkfreshdir(self, d):
+        if d.find('/tmp/') == -1:
+            raise RuntimeError, 'unsafe call: mkfreshdir(%s)' % d
+
+        cwd = os.getcwd()
+        os.chdir('/')
+        if os.path.exists(d):
+            shutil.rmtree(d)
+        os.makedirs(d)
+        os.chdir(cwd)
+
+    def calcPaths(self):
+        self.testname = self._testMethodName
+        self.test_dir = os.path.join(self.tmpdir, self.testname)
+        self.pkgdir   = os.path.join(self.test_dir, 'pkg')
+        self.outdir   = os.path.join(self.test_dir, 'out')
+
+    def setUp(self):
+        self.calcPaths()
+        self.initDirs()
+
+        # osc launches source services with cwd as pkg dir
+        os.chdir(self.pkgdir)
+
+    def initDirs(self):
+        # pkgdir persists between tests (although a test can invoke mkfreshdir)
+        if not os.path.exists(self.pkgdir):
+            os.makedirs(self.pkgdir)
+
+    def tearDown(self):
+        self.postRun()
+
+    def postRun(self):
+        # Simulate osc copying files from temporary --outdir back to
+        # package source directory, so our tests can catch any
+        # potential side-effects due to the persistent nature of the
+        # package source directory.
+
+        self.service = { 'mode' : 'disabled' }
+        temp_dir = self.outdir
+        dir = self.pkgdir
+        service = self.service
+
+        # This code copied straight out of osc/core.py Serviceinfo.execute()
+        if service['mode'] == "disabled" or service['mode'] == "trylocal" or service['mode'] == "localonly" or callmode == "local" or callmode == "trylocal":
+            for filename in os.listdir(temp_dir):
+                shutil.move( os.path.join(temp_dir, filename), os.path.join(dir, filename) )
+        else:
+            for filename in os.listdir(temp_dir):
+                shutil.move( os.path.join(temp_dir, filename), os.path.join(dir, "_service:"+name+":"+filename) )
+                
+        self.reset_upstream('ten')
+
+    def assertNumDirents(self, dir, expected, msg = ''):
+        dirents = os.listdir(dir)
+        got = len(dirents)
+        if len(msg) > 0: msg += "\n"
+        msg += 'expected %d file(s), got %d: %s' % (expected, got, pformat(dirents))
+        self.assertEqual(expected, got, msg)
+        return dirents
+
+    def assertNumTarEnts(self, tar, expected, msg = ''):
+        self.assertTrue(tarfile.is_tarfile(tar))
+        t = tarfile.open(tar)
+        tarents = t.getmembers()
+        got = len(tarents)
+        if len(msg) > 0: msg += "\n"
+        msg += 'expected %s to have %d entries, got %d:\n%s' % \
+            (tar, expected, got, pformat(tarents))
+        self.assertEqual(expected, got, msg)
+        return tarents
+
+    def assertStandardTar(self, tar, top):
+        entries = self.assertNumTarEnts(tar, 4)
+        pprint(entries)
+        self.assertEqual(entries[0].name, top)
+        self.assertEqual(entries[1].name, top + '/a')
+        self.assertEqual(entries[2].name, top + '/subdir')
+        self.assertEqual(entries[3].name, top + '/subdir/b')
+
+    def assertSubdirTar(self, tar, top):
+        entries = self.assertNumTarEnts(tar, 2)
+        self.assertEqual(entries[0].name, top)
+        self.assertEqual(entries[1].name, top + '/b')
+
+    def assertTarOnly(self, tarbasename, toptardir=None, tarchecker=None):
+        if not toptardir:  toptardir = tarbasename
+        if not tarchecker: tarchecker = self.assertStandardTar
+
+        dirents = self.assertNumDirents(self.outdir, 1)
+        self.assertEqual(dirents[0], '%s.tar' % tarbasename)
+        tar = os.path.join(self.outdir, dirents[0])
+        tarchecker(tar, toptardir)
+
+    def assertTarAndDir(self, tarbasename, toptardir=None, tarchecker=None, dirname=None):
+        if not toptardir:  toptardir = tarbasename
+        if not tarchecker: tarchecker = self.assertStandardTar
+        if not dirname:    dirname = tarbasename
+
+        dirents = self.assertNumDirents(self.outdir, 2)
+        pprint(dirents)
+
+        if dirents[0][-4:] == '.tar':
+            tar = dirents[0]
+            wd  = dirents[1]
+        elif dirents[1][-4:] == '.tar':
+            tar = dirents[1]
+            wd  = dirents[0]
+        else:
+            self.fail('no .tar found in ' + self.outdir)
+
+        self.assertEqual(wd, dirname)
+        self.assertTrue(os.path.isdir(os.path.join(self.outdir, wd)),
+                        dirname + ' should be directory')
+
+        self.assertEqual(tar, '%s.tar' % tarbasename)
+        tarpath = os.path.join(self.outdir, tar)
+        self.assertStandardTar(tarpath, toptardir)
+
+    def tar_scm_std(self, *args, **kwargs):
+        return self.tar_scm(self.stdargs(*args), **kwargs)
+
+    def tar_scm_std_fail(self, *args):
+        return self.tar_scm(self.stdargs(*args), should_succeed=False)
+
+    def tar_scm(self, args, should_succeed=True):
+        # simulate new temporary outdir for each tar_scm invocation
+        self.mkfreshdir(self.outdir)
+        cmd = [ self.tar_scm_bin ] + args + [ '--outdir', self.outdir ]
+        cmdstr = " ".join(cmd) + " 2>&1"
+        print "\n"
+        print "-" * 70
+        print "Running", cmdstr
+        print "Output:"
+        print "------"
+        (stdout, stderr, ret) = run_cmd(cmdstr)
+        print stdout,
+        # print "------"
+        # print stderr,
+        print "------"
+        succeeded = ret == 0
+        self.assertEqual(succeeded, should_succeed)
+        return (stdout, stderr, ret)
