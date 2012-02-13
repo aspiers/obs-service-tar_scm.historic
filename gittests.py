7725cf6afdaedb848d4472cf2b40ae4f1b6ccf77
Adam Spiers <aspiers@suse.com>
2012-02-13 19:30:42 +0000

    Add test suite and fix some bugs:
    1. --subdir didn't work with --scm svn
    2. --scm bzr didn't work

---
+++ gittests.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from   unittest    import expectedFailure
+
+from   commontests import CommonTests
+from   gitfixtures import GitFixtures
+from   utils       import run_git
+
+class GitTests(CommonTests):
+    scm = 'git'
+    initial_clone_command = 'git clone'
+    update_cache_command  = 'git fetch'
+    fixtures_class = GitFixtures
+
+    def default_version(self):
+        return self.timestamps(self.rev(2))
+
