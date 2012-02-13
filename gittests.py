cc3e408f4e22fef58ff16cb45c8c6b425409eb3a
Adam Spiers <aspiers@suse.com>
2012-02-13 15:00:37 +0000

    Add new 'versionformat' option and support caching of cloned repositories.

--- gittests.py
+++ gittests.py
@@ -17,3 +17,28 @@ class GitTests(CommonTests):
     def default_version(self):
         return self.timestamps(self.rev(2))
 
+    def test_versionformat_abbrevhash(self):
+        self.tar_scm_std('--versionformat', '%h')
+        self.assertTarOnly(self.basename(version = self.sha1s(self.rev(2))))
+
+    def test_versionformat_timestamp(self):
+        self.tar_scm_std('--versionformat', '%at')
+        self.assertTarOnly(self.basename(version = self.timestamps(self.rev(2))))
+
+    def test_versionformat_mixed(self):
+        self.tar_scm_std('--versionformat', '%at.master.%h')
+        version = '%s.master.%s' % (self.timestamps(self.rev(2)), self.sha1s(self.rev(2)))
+        self.assertTarOnly(self.basename(version = version))
+
+    def test_version_versionformat(self):
+        self.tar_scm_std('--version', '3.0', '--versionformat', '%at.master.%h')
+        version = '%s.master.%s' % (self.timestamps(self.rev(2)), self.sha1s(self.rev(2)))
+        self.assertTarOnly(self.basename(version=version))
+
+    def test_versionformat_revision(self):
+        self.fixtures.create_commits(4)
+        self.tar_scm_std('--versionformat', '%h', '--revision', self.rev(2))
+        basename = self.basename(version = self.sha1s(self.rev(2)))
+        th = self.assertTarOnly(basename)
+        self.assertTarMemberContains(th, basename + '/a', '2')
+
